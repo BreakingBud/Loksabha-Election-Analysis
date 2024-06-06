d4cefc94172fb8efb5a1c4ec95a056d32a003c5f
BreakingBud <68956272+BreakingBud@users.noreply.github.com>
2024-06-06 19:35:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -39,10 +39,6 @@ st.title('Loksabha Election Results 2024 - Seats Won by Each Party')
 # Treemap of number of seats won by each party using plotly
 fig = px.treemap(party_seats, path=['Party Abbreviation'], values='Seats', color='Seats',
                  color_continuous_scale='Viridis', title='Number of Seats Won by Each Party',
-                 hover_data={'Winning Party': True, 'Seats': True},
-                 width=800, height=600)
-
-# Make sure to show the full name when hovered
-fig.update_traces(textinfo='label+value+percent parent+percent entry', insidetextorientation='radial')
+                 hover_data={'Winning Party': True, 'Seats': True})
 
 st.plotly_chart(fig)
