11477110a8260fce507411b56001ba519d7538c8
BreakingBud <68956272+BreakingBud@users.noreply.github.com>
2024-06-06 19:16:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,20 @@ data = pd.read_csv(file_path)
 # Data preprocessing
 data['Margin Votes'] = data['Margin Votes'].str.replace(',', '').replace('-', '0').astype(int)
 
+# Generate abbreviations for each party
+def generate_abbreviation(name):
+    words = name.split()
+    if len(words) == 1:
+        return name[:3].upper()
+    else:
+        return ''.join([word[0] for word in words]).upper()
+
+party_names = data['Winning Party'].unique()
+party_abbreviations = {name: generate_abbreviation(name) for name in party_names}
+
+# Apply the abbreviations to the data
+data['Winning Party'] = data['Winning Party'].map(party_abbreviations).fillna(data['Winning Party'])
+
 # Calculate the number of seats won by each party
 party_seats = data['Winning Party'].value_counts().reset_index()
 party_seats.columns = ['Winning Party', 'Seats']
@@ -19,7 +33,8 @@ st.title('Loksabha Election Results 2024 - Seats Won by Each Party')
 
 # Treemap of number of seats won by each party
 fig, ax = plt.subplots(1, figsize=(16, 10))
-squarify.plot(sizes=party_seats['Seats'], label=party_seats['Winning Party'], alpha=.8, ax=ax, color=plt.cm.Blues(range(len(party_seats))))
+colors = plt.cm.tab20(range(len(party_seats)))
+squarify.plot(sizes=party_seats['Seats'], label=party_seats['Winning Party'], alpha=.8, ax=ax, color=colors)
 ax.set_title('Number of Seats Won by Each Party', fontsize=20)
 ax.axis('off')
 st.pyplot(fig)
